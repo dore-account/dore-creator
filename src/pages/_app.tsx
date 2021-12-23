8c9b56335ba792c477875bc6739b42d8e6c190ac
hbk2002320@gmail.com <hbk2002320@gmail.com>
2021-12-24 00:59:37 +0900

    feat: apollo clientの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import '../styles/globals.css'
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
+  cache,
+})
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   )
 }
