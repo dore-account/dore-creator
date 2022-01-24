af5118dbb9cc13a51f860081fe0dca05a45f9441
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-01-25 02:19:15 +0900

    feat: header right left

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,22 +1,30 @@
-import { Flex, HStack, Box, Center, Heading } from '@chakra-ui/react';
-import React from 'react';
+import { Flex, HStack, Box, Center, Heading } from '@chakra-ui/react'
+import React from 'react'
 
 export type HeaderProps = {
-  title: string;
-};
+  title: string
+  rightComponent: any
+  leftComponent: any
+}
 
-export const Header: React.FC<HeaderProps> = ({ title }) => {
-  return <Flex w="full" shadow="sm" position="sticky">
-  <HStack flex="1" h="full" px="4">
-    <Box w="0" position="relative">
-      <Center left="0" h="full" position="absolute" />
-    </Box>
-    <Center h="14" w={{ base: 'full', md: 'unset' }}>
-      <Heading size="md">{title}</Heading>
-    </Center>
-    <Box w="0" position="relative">
-      <Center right="0" h="full" position="absolute" />
-    </Box>
-  </HStack>
-</Flex>;
-};
+export const Header: React.FC<HeaderProps> = ({ title, rightComponent, leftComponent }) => {
+  return (
+    <Flex w="full" shadow="sm" position="sticky">
+      <HStack flex="1" h="full" px="4">
+        <Box w="0" position="relative">
+          <Center left="0" h="full" position="absolute" >
+            {leftComponent}
+          </Center>
+        </Box>
+        <Center h="14" w={{ base: 'full', md: 'unset' }}>
+          <Heading size="md">{title}</Heading>
+        </Center>
+        <Box w="0" position="relative">
+          <Center right="0" h="full" position="absolute">
+            {rightComponent}
+          </Center>
+        </Box>
+      </HStack>
+    </Flex>
+  )
+}
