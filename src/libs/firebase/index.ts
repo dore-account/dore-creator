613cd31f91af6b833077b135931dfb4a7e51d046
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-02-04 06:49:30 +0900

    fix: typo

--- src/libs/firebase/index.ts
+++ src/libs/firebase/index.ts
@@ -2,27 +2,25 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 
 export const config = {
-  apiKey: process.env.FIREBASE_KEY,
-  authDomain: process.env.FIREBASE_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 const getFirebaseApp = (): firebase.app.App | undefined => {
   if (typeof window === 'undefined') return
 
   if (!firebase.apps.length) {
-    console.log('initialize');
+    console.log('initialize')
     return firebase.initializeApp(config)
   } else {
-    console.log('not initialize');
+    console.log('not initialize')
     return firebase.app()
   }
 }
 
-const firebaseAuth = firebase.auth(getFirebaseApp())
-
-export { getFirebaseApp, firebaseAuth, firebase }
+export { getFirebaseApp, firebase }
