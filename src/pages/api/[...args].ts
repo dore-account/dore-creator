25ecc801610b28238b0e31ce9a5b75a4e71b45a0
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-02-08 21:42:27 +0900

    feat: proxy

---
+++ src/pages/api/[...args].ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import httpProxyMiddleware from 'next-http-proxy-middleware';
+
+const proxy = (req: NextApiRequest, res: NextApiResponse) =>
+  httpProxyMiddleware(req, res, {
+    target: process.env.NEXT_PUBLIC_BACKEND_URL,
+    changeOrigin: true,
+    pathRewrite: [
+      {
+        patternStr: '^/api/graphql',
+        replaceStr: '/graphql',
+      },
+    ],
+    prependPath: true,
+  });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default proxy;
