6a6d3433d71cbf474e65e8cf0e772076fe29af48
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-02-26 18:27:22 +0900

    feat: firebase auth logic

--- src/components/auth/useAuthForm.ts
+++ src/components/auth/useAuthForm.ts
@@ -1,5 +1,7 @@
+import { useRouter } from 'next/router'
 import { useContext } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { useAuthContext } from 'src/hooks/auth/useAuthState'
 import { AuthFormContext } from './AuthForm'
 
 export const useAuthFormContext = () => useContext(AuthFormContext)
@@ -10,9 +12,25 @@ export const useAuthForm = () => {
     formState: { errors, isSubmitting, isValid },
     handleSubmit,
   } = useForm({ mode: 'all' })
+  const { signInWithEmail, signUpWithEmail, passwordReset } = useAuthContext()
+  const router = useRouter()
 
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const signIn: SubmitHandler<FieldValues> = async (data) => {
     console.log(data)
+    const success = await signInWithEmail(data.email, data.password)
+    if (success) router.push('/')
+  }
+
+  const signUp: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data)
+    const success = await signUpWithEmail(data.email, data.password)
+    if (success) router.push('/profile/register')
+  }
+
+  const sendPasswordReset: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data)
+    const success = await passwordReset(data.email)
+    if (success) router.back()
   }
 
   return {
@@ -20,6 +38,9 @@ export const useAuthForm = () => {
     errors,
     isSubmitting,
     isValid,
-    handleSubmit: handleSubmit(onSubmit),
+    handleSubmit,
+    signIn,
+    signUp,
+    sendPasswordReset,
   }
 }
--- src/hooks/auth/useAuthState.tsx
+++ src/hooks/auth/useAuthState.tsx
@@ -1,4 +1,11 @@
-import { getIdToken, onIdTokenChanged, User } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  getIdToken,
+  onIdTokenChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  User,
+} from 'firebase/auth'
 import { useRouter } from 'next/router'
 import { createContext, useContext, useEffect, useState } from 'react'
 import nookies from 'nookies'
@@ -10,15 +17,22 @@ export type AuthState = {
   user: User | null
 }
 
+type Auth = {
+  authState: AuthState
+  signInWithEmail: (email: string, password: string) => Promise<boolean>
+  signUpWithEmail: (email: string, password: string) => Promise<boolean>
+  passwordReset: (email: string) => Promise<boolean>
+}
+
 const INITIAL_AUTH_STATE = {
   isSignedIn: false,
   isLoading: true,
   user: null,
 }
 
-const PublicPaths: string[] = ['/signin', '/signup']
+const PublicPaths: string[] = ['/signin', '/signup', '/sendPassword']
 
-const AuthContext = createContext<AuthState>({} as AuthState)
+const AuthContext = createContext<Auth>({} as Auth)
 
 export const useAuthContext = () => {
   return useContext(AuthContext)
@@ -28,19 +42,23 @@ const useAuthState = () => {
   const [authState, setAuthState] = useState<AuthState>(INITIAL_AUTH_STATE)
   const router = useRouter()
 
+  const setIdToken = async (user: User) => {
+    const idToken = await getIdToken(user)
+    // jwtをcookieに保存
+    nookies.set(null, 'ID_TOKEN', idToken, {})
+    setAuthState({
+      isSignedIn: true,
+      isLoading: false,
+      user: user,
+    })
+  }
+
   useEffect(() => {
     const unsubscribe = onIdTokenChanged(
       firebase.auth(getFirebaseApp()),
       async (user) => {
         if (user) {
-          const idToken = await getIdToken(user)
-          // jwtをcookieに保存
-          nookies.set(null, 'ID_TOKEN', idToken, {})
-          setAuthState({
-            isSignedIn: true,
-            isLoading: false,
-            user: user,
-          })
+          setIdToken(user)
         } else {
           // cookiesを削除
           nookies.destroy(null, 'ID_TOKEN')
@@ -67,7 +85,69 @@ const useAuthState = () => {
     }
   }, [authState.isLoading, authState.user, router])
 
-  return authState
+  const signInWithEmail = async (
+    email: string,
+    password: string
+  ): Promise<boolean> => {
+    setAuthState({ ...authState, isLoading: true })
+    const result = async () => {
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+          firebase.auth(getFirebaseApp()),
+          email,
+          password
+        )
+        await setIdToken(userCredential.user)
+        return true
+      } catch {
+        return false
+      }
+    }
+
+    setAuthState({ ...authState, isLoading: false })
+    return result()
+  }
+
+  const signUpWithEmail = async (
+    email: string,
+    password: string
+  ): Promise<boolean> => {
+    setAuthState({ ...authState, isLoading: true })
+    const result = async () => {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(
+          firebase.auth(getFirebaseApp()),
+          email,
+          password
+        )
+        await setIdToken(userCredential.user)
+        return true
+      } catch {
+        return false
+      }
+    }
+
+    setAuthState({ ...authState, isLoading: false })
+    return result()
+  }
+
+  const passwordReset = async (email: string): Promise<boolean> => {
+    const resutl = await sendPasswordResetEmail(
+      firebase.auth(getFirebaseApp()),
+      email
+    )
+      .then(() => {
+        return true
+      })
+      .catch((e) => {
+        console.log(e.message)
+        return false
+      })
+
+    return resutl
+  }
+
+  return { authState, signInWithEmail, signUpWithEmail, passwordReset }
 }
 
 export const AuthProvider: React.FC = ({ children }) => {
